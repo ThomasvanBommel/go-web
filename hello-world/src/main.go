ed01b260dbb2539a299c8c80ed0c6ee8283a320e
ThomasvanBommel <thomas@vanbommel.ca>
2022-03-06 13:34:32 -0400

    Added makefile + gitignore

--- hello-world/src/main.go
+++ hello-world/src/main.go
@@ -1,4 +1,13 @@
-// hello-world
-// Thomas vanBommel
-// 2022-03-06
+/*
+ * hello-world
+ * Thomas vanBommel
+ * 2022-03-06
+ */
 
+package main
+
+import ( "fmt" )
+
+func main() {
+	fmt.Println("Hello World!")
+}
