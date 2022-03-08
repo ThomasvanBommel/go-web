7e09bdc3e83dde0f62614a749da9db7239ede35e
ThomasvanBommel <thomas@vanbommel.ca>
2022-03-07 21:00:24 -0400

    Finished code + updated README.md

--- hello-world/README.md
+++ hello-world/README.md
@@ -1,2 +1,26 @@
 # [Hello-World](https://gowebexamples.com/hello-world/)
 Simple web response
+
+# How to run
+Run without building
+```bash
+make run
+```
+<br/>
+
+Build
+```bash
+make build
+```
+<br/>
+
+Run built code
+```bash
+bin/main
+```
+<br/>
+
+Build & Run
+```bash
+make build && bin/main
+```
--- hello-world/src/main.go
+++ hello-world/src/main.go
@@ -6,8 +6,17 @@
 
 package main
 
-import ( "fmt" )
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Listening http://localhost:8080")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
